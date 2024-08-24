df323fe1ea916817da2aff0f293cc75f44bb6551
jirasak <jirasakwaennak1776@gmail.com>
2024-08-24 15:56:10 +0700

    flutter1

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_aten/homelogin/login.dart'; // Assuming this file exists
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
